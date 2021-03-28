63803699e51fbb7d4bb74be0761b820889f9d088
RIKU <rank228130@gmail.com>
2021-03-29 08:02:35 +0900

    Create login/logout

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -15,15 +15,15 @@
   <form class="form-signin" method="POST" action="{{route('login')}}">
     @csrf
     <h1 class="h3 mb-3 font-weight-normal">ログインフォーム</h1>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-      <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-      </ul>
-    </div>
-    @endif
+    @foreach ($errors->all() as $error)
+    <ul>
+      <li>{{ $error }}</li>
+    </ul>
+    @endforeach
+
+    <x-alert type="danger" :session="session('danger')" />
+    <!-- <x-alert type="danger" :session="session('logout')" /> -->
+
     <label for="inputEmail" class="sr-only">Email address</label>
     <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
