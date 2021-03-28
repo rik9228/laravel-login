589159a51a882aede8debc0032fb4d04cf098c51
RIKU <rank228130@gmail.com>
2021-03-28 18:16:38 +0900

    ログインフォーム作成、バリデーション作成

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,9 +11,10 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|ll
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ログイン画面のルートをセット
+Route::get('/',[AuthController::class,'showLogin'])->name('showLogin');
+// ログイン処理のルートをセット
+Route::post('login',[AuthController::class, 'login'])->name('login');
